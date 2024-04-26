a60a34c799aa72c858c8b8a6a675efea7357c054
Bishnu <khanalbishnu333@gmail.com>
2024-04-26 08:10:03 +0545

    product edit and delete completed

--- src/product/ProductList.jsx
+++ src/product/ProductList.jsx
@@ -1,11 +1,8 @@
 import React, { useEffect, useState } from "react";
 import AuthUser from "../AuthUser";
 import Spinner from "../components/Spinner";
-import Modal from 'react-bootstrap/Modal';
-import Button from 'react-bootstrap/Button';
 import ProductTableRow from "./ProductTableRow";
-
-
+import { toast } from "react-toastify";
 
 function ProductList() {
   const productURL = "/dashboard/products";
@@ -13,10 +10,6 @@ function ProductList() {
   const [loading, setLoading] = useState(true);
 
   const { http } = AuthUser();
-  // bootstrap modal 
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
   useEffect(() => {
     getProductList();
   }, []);
@@ -27,6 +20,7 @@ function ProductList() {
         if (data.response) {
           setProducts(data.products);
         } else {
+          toast.success(data.message);
           console.log(data.message || "Something went wrong!");
         }
         setLoading(false);
@@ -35,20 +29,70 @@ function ProductList() {
       console.log(error);
     }
   };
-  const handleUpdate = (productId,formData) => {
-    console.log(formData);
-    debugger
-};
+  // update function for product
+  const handleUpdate = (productId, formData) => {
+    let newFormData = { ...formData, id: productId };
+    setLoading(true);
+    http.post(`${productURL}/update`, newFormData).then((res) => {
+      let data = res.data;
+      setLoading(false);
+      if (data.response) {
+        toast.success(data.message);
+        getProductList();
+        // setProducts(products.map((product) => {
+        //   if (product.id === productId) {
+        //     return { ...product, ...newFormData };
+        //   }
+        //   return product;
+        // }));
+      } else if (data.message) {
+        if (typeof data.message === "object") {
+          Object.keys(data.message).forEach((key) => {
+            data.message[key].forEach((err) => {
+              toast.error(err);
+            });
+          });
+        }
+        if (typeof data.message == "string") {
+          toast.error(data.message);
+        }
+      }
+    });
+  };
+
+  const handleDeleteProduct=(productId)=>{
+    console.log(productId);
+    setLoading(true);
+    http.get(`${productURL}/delete/`+productId).then((res)=>{
+      let data=res.data;
+      setLoading(false);
+      if(data.response){
+        toast.success(data.message);
+        setProducts(products.filter((product) => product.id !== productId));
+      }else{
+        toast.error(data.message);
+      }
+    })
+  }
 
   const renderListOfProducts = (products) => {
     return products?.map((product, index) => (
-      <ProductTableRow key={product.id} index={index} product={product} onUpdate={handleUpdate} />
+      <ProductTableRow
+        key={product.id}
+        index={index}
+        product={product}
+        onUpdate={handleUpdate}
+        onDelete={handleDeleteProduct}
+      />
     ));
   };
   return (
-    <div className="p-4 bg-gradient" style={{ height: "92vh",overflow:'auto' }}>
+    <div
+      className="p-4 bg-gradient"
+      style={{ height: "92vh", overflow: "auto" }}
+    >
       {loading ? (
-       <Spinner />
+        <Spinner />
       ) : (
         <table className="table table-bordered table-striped ">
           <thead>
@@ -67,5 +111,4 @@ function ProductList() {
     </div>
   );
 }
-
 export default ProductList;
--- src/product/ProductTableRow.jsx
+++ src/product/ProductTableRow.jsx
@@ -1,112 +1,169 @@
-import React, { useState } from 'react';
-import { Modal, Button, Form } from 'react-bootstrap';
+import React, { useState } from "react";
+import { Modal, Button, Form } from "react-bootstrap";
+import { toast } from "react-toastify";
 
-const ProductTableRow = ({ product, onUpdate,index }) => {
-    const [showModal, setShowModal] = useState(false);
-    const [formData, setFormData] = useState({
-        title: product.title,
-        description: product.description,
-        tags: product.tags,
-        // file: product.media.length>0 ? product.media[0].original_url:null
-    });
-    const [preview,setPreview]=useState(null);
+const ProductTableRow = ({ product, onUpdate, index,onDelete }) => {
+  const [showModal, setShowModal] = useState(false);
+  const [formData, setFormData] = useState({
+    title: product.title || "",
+    description: product.description || "",
+    tags: product.tags || "",
+    // file: product.media.length>0 ? product.media[0].original_url:null
+  });
+  const [preview, setPreview] = useState(null);
 
+  const handleCloseModal = () => setShowModal(false);
+  const handleOpenModal = () => setShowModal(true);
 
-    const handleCloseModal = () => setShowModal(false);
-    const handleOpenModal = () => setShowModal(true);
+  const handleChange = (e) => {
+    const { name } = e.target;
+    let fieldValue;
+    if (name == "file") {
+      fieldValue = e.target.files[0];
+      if (
+        fieldValue &&
+        !["image/jpeg", "image/png"].includes(fieldValue.type)
+      ) {
+        fieldValue = null;
+        setErrors({ ...errors, file: "File must be a JPG or PNG image." });
+        setPreview("");
+      } else {
+        setErrors({ ...errors, file: null });
+        setPreview(URL.createObjectURL(fieldValue));
+      }
+    } else {
+      fieldValue = e.target.value;
+    }
+    setFormData({ ...formData, [name]: fieldValue });
+  };
 
-    const handleChange = (e) => {
-        const { name} = e.target;
-        let fieldValue;
-        if(name=="file"){
-             fieldValue=e.target.files[0];
-             setPreview(URL.createObjectURL(fieldValue));
-        }else{
-            fieldValue=e.target.value;
-        }
-        setFormData({ ...formData, [name]: fieldValue });
-    };
+  const [errors, setErrors] = useState({});
 
-    const handleUpdate = () => {
-        onUpdate(product.id, formData);
-        handleCloseModal();
-    };
-    return (
-        <tr>
-            <td>{index+1}</td>
-            <td>{product.title}</td>
-            <td>
-            {
-             product.media.length>0 && <img src={product.media[0].original_url} height="80px" width="100px" alt="" srcSet="" />
-            }
-            </td>
-            <td>{product.tags}</td>
-            <td>
-                <Button variant="primary" onClick={handleOpenModal} className='mx-1'>
-                    Edit
-                </Button>
-                <Button variant="danger">
-                    Delete
-                </Button>
-            </td>
-            <Modal show={showModal} onHide={handleCloseModal}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Edit Product</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>
-                    <Form>
-                        <Form.Group controlId="formTitle">
-                            <Form.Label>Title</Form.Label>
-                            <Form.Control
-                                type="text"
-                                name="title"
-                                value={formData.title}
-                                onChange={handleChange}
-                            />
-                        </Form.Group>
-                        <Form.Group controlId="formDescription">
-                            <Form.Label>Description</Form.Label>
-                            <Form.Control
-                                as="textarea"
-                                rows="4"
-                                name="description"
-                                value={formData.description}
-                                onChange={handleChange}
-                            />
-                        </Form.Group>
-                        <Form.Group controlId="formTags">
-                            <Form.Label>Tags</Form.Label>
-                            <Form.Control
-                                type="text"
-                                name="tags"
-                                value={formData.tags}
-                                onChange={handleChange}
-                            />
-                        </Form.Group>
-                        <Form.Group controlId="formFile">
-                            <Form.Label>File</Form.Label>
-                            <Form.Control
-                                type="file"
-                                name="file"
-                                onChange={handleChange}
-                            />
-                        {
-                            product.media.length>0 && preview==null ? (<img src={  product.media[0].original_url} height="80px" width="100px" alt="" srcSet="" />):(<img src={preview} height="80px" width="100px" alt="" srcSet="" />)
-                        }
-                        </Form.Group>
-                    </Form>
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button variant="secondary" onClick={handleCloseModal}>
-                        Close
-                    </Button>
-                    <Button variant="primary" onClick={handleUpdate}>
-                        Save Changes
-                    </Button>
-                </Modal.Footer>
-            </Modal>
-        </tr>
-    );
+  const handleUpdate = () => {
+    const formError = {};
+    if (!formData.title.trim()) {
+      formError.title = "Title is required!";
+    }
+    if (!formData.description.trim()) {
+      formError.description = "Title is required!";
+    }
+    if (errors.file) {
+      formError.file = errors.file;
+    }
+    setErrors(formError);
+    if (Object.keys(formError).length === 0) {
+      onUpdate(product.id, formData);
+      handleCloseModal();
+    }
+  };
+
+  const handleDeleteProduct = () => {
+    if (window.confirm("Are you sure you want to delete this item?")) {
+        onDelete(product.id);
+    }
+  };
+  return (
+    <tr>
+      <td>{index + 1}</td>
+      <td>{product.title}</td>
+      <td>
+        {product.media.length > 0 && (
+          <img
+            src={product.media[0].original_url}
+            height="80px"
+            width="100px"
+            alt=""
+            srcSet=""
+          />
+        )}
+      </td>
+      <td>{product.tags}</td>
+      <td>
+        <Button variant="primary" onClick={handleOpenModal} className="mx-1">
+          Edit
+        </Button>
+        <Button variant="danger" onClick={handleDeleteProduct}>
+          Delete
+        </Button>
+      </td>
+      <Modal show={showModal} onHide={handleCloseModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Edit Product</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formTitle">
+              <Form.Label>Title</Form.Label>
+              <Form.Control
+                type="text"
+                name="title"
+                value={formData.title}
+                onChange={handleChange}
+                className={`${
+                  errors.title ? "border-danger" : ""
+                } border rounded`}
+              />
+            </Form.Group>
+            <Form.Group controlId="formDescription">
+              <Form.Label>Description</Form.Label>
+              <Form.Control
+                as="textarea"
+                rows="4"
+                name="description"
+                value={formData.description}
+                onChange={handleChange}
+                className={`${
+                  errors.description ? "border-danger" : ""
+                } border rounded`}
+              />
+            </Form.Group>
+            <Form.Group controlId="formTags">
+              <Form.Label>Tags</Form.Label>
+              <Form.Control
+                type="text"
+                name="tags"
+                value={formData.tags}
+                onChange={handleChange}
+                className="border rounded "
+              />
+            </Form.Group>
+            <Form.Group controlId="formFile">
+              <Form.Label>File</Form.Label>
+              <Form.Control type="file" name="file" onChange={handleChange} />
+              {product.media.length > 0 && preview == null ? (
+                <img
+                  src={product.media[0].original_url}
+                  height="80px"
+                  width="100px"
+                  alt=""
+                  srcSet=""
+                />
+              ) : (
+                <img
+                  src={preview}
+                  height="80px"
+                  width="100px"
+                  alt=""
+                  srcSet=""
+                />
+              )}
+              {errors.file && (
+                <span className="text-danger">{errors.file}</span>
+              )}
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseModal}>
+            Cancel
+          </Button>
+          <Button variant="primary" onClick={handleUpdate}>
+            Update
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </tr>
+  );
 };
 
 export default ProductTableRow;
