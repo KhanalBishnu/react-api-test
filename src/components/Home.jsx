7c7d5f5e8aa8115662c950b4cf7326df6c942852
bishnu <khanalbishnu333@gmail.com>
2024-05-30 19:53:42 +1200

    scroll wise data list

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,21 +4,48 @@ import AuthUser from '../AuthUser'
 import Spinner from './Spinner';
 import { Link } from 'react-router-dom';
 function Home() {
-  const {http}=AuthUser();
-  const [products,setProducts]=useState([]);
-  const [loading,setLoading]=useState(true);
+  const { http } = AuthUser();
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [pageNumber, setPageNumber] = useState(null);
+  const [limit, setLimit] = useState(8);
+  let newPage=1;
 
+  useEffect(() => {
+    getProductLIst();
+    console.log(pageNumber);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
 
-  useEffect(()=>{
-    http.post('/products').then((res)=>{
+  }, []);
+
+  const getProductLIst = async (page=1) => {
+    debugger
+    await  http.post('/products', { page, limit }).then((res) => {
       console.log(res);
       setLoading(false)
-      setProducts(res.data.data.products);
+      setProducts(prevItems => [...prevItems, ...res.data.data.products]);
+     
+    }).catch(function (error) {
+      console.error(error);
+    });
+    setPageNumber(prev=>prev+1);
 
-    })
-  },[]);
 
-  const truncateText= (text, wordLimit) => {
+  }
+  function handleScroll(e) {
+    e.preventDefault();
+    if (
+      window.innerHeight + e.target.documentElement.scrollTop + 1 >=
+      e.target.documentElement.scrollHeight
+    ) {
+      // setPageNumber(prev=>prev+1);
+      newPage=newPage+1;
+      getProductLIst(newPage)
+    }
+  }
+
+  const truncateText = (text, wordLimit) => {
     const words = text.split(' ');
     if (words.length <= wordLimit) {
       return text;
@@ -26,11 +53,11 @@ function Home() {
     return words.slice(0, wordLimit).join(' ') + '...';
   };
 
-  const  TagBadges=(tags) =>{
+  const TagBadges = (tags) => {
     const tagsArray = tags.split(',');
     return tagsArray;
   }
-  const tagColors = ['bg-info', 'bg-secondary', 'bg-success','bg-primary', 'bg-danger', 'bg-warning',  'bg-light', 'bg-dark'];
+  const tagColors = ['bg-info', 'bg-secondary', 'bg-success', 'bg-primary', 'bg-danger', 'bg-warning', 'bg-light', 'bg-dark'];
 
   return (
     <>
@@ -42,31 +69,31 @@ function Home() {
         </div>
         {loading ?
           <Spinner content="Loading..." /> :
-          <div className="row">
-              {
-                products?.map((product)=>(
-                  <div className="col-lg-3 col-md-4  col-sm-6 d-flex align-items-stretch " key={product.id}>
-                        <div className="card m-2 home-image-container">
-                        <Link to={`/product/details/${product.id}`}>
-                      <img src={product.media.length>0?product.media[0].original_url:"/images/test.jpeg"} className="card-img-top" alt="product-images"height="280px" />
-                      </Link>
-                      <div className="card-body">
-                        <h5 className="card-title">{product.title}</h5>
-                        <p className="card-text">{truncateText(product.description,8)}</p>
-                        {
-                          TagBadges(product.tags)?.map((tag, index) => (
-                            <span key={index} className={`badge rounded-pill text-dark m-2 p-1  px-2 ${tagColors[index % tagColors.length]}`}>
-                              {tag}
-                            </span>
-                          ))
-                        }<br />
+          <div className="row mt-2">
+            {
+              products?.map((product) => (
+                <div className="col-lg-3 col-md-4  col-sm-6 d-flex align-items-stretch " key={product.id}>
+                  <div className="card m-2 home-image-container">
+                    <Link to={`/product/details/${product.id}`}>
+                      <img src={product.media.length > 0 ? product.media[0].original_url : "/images/test.jpeg"} className="card-img-top" alt="product-images" height="280px" />
+                    </Link>
+                    <div className="card-body">
+                      <h5 className="card-title">{product.title}</h5>
+                      <p className="card-text">{truncateText(product.description, 8)}</p>
+                      {
+                        TagBadges(product.tags)?.map((tag, index) => (
+                          <span key={index} className={`badge rounded-pill text-dark m-2 p-1  px-2 ${tagColors[index % tagColors.length]}`}>
+                            {tag}
+                          </span>
+                        ))
+                      }<br />
 
-                        <Link to={`/product/details/${product.id}`} className="btn btn-primary btn-sm">Read Me</Link>
-                      </div>
+                      <Link to={`/product/details/${product.id}`} className="btn btn-primary btn-sm">Read Me</Link>
                     </div>
                   </div>
-                ))
-              }
+                </div>
+              ))
+            }
           </div>
         }
       </div >
--- src/main.jsx
+++ src/main.jsx
@@ -11,12 +11,12 @@ import store from './app/store'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  // <React.StrictMode>
       <Provider store={store}>
         <BrowserRouter>
             <App />
             <ToastContainer />
         </BrowserRouter>
         </Provider>
-  </React.StrictMode>
+  // </React.StrictMode>
 );
